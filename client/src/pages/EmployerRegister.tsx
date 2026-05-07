3ecf3b676fea3c8232e8dd52f78fa2e827d431b2
wiezpho <138117984+wiezpho@users.noreply.github.com>
2026-05-08 01:52:59 +0700

    fix: add useEffect to React import

--- client/src/pages/EmployerRegister.tsx
+++ client/src/pages/EmployerRegister.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useLocation } from "wouter";
 import { toast } from "sonner";
 import BoothMapPicker, { ALL_BOOTHS } from "@/components/BoothMapPicker";
